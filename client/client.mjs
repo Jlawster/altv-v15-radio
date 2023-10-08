fd7856d45b2668290189286ceed7b2d1b9efbc86
Jlawster <jordan.sellam.ort@gmail.com>
2023-10-08 12:28:40 +0200

    edits

---
+++ client/client.mjs
@@ -0,0 +1,135 @@
+import * as alt from 'alt';
+import * as native from 'natives';
+
+let browser = undefined;
+let pedInSeat = undefined;
+let mounted = false;
+let player = alt.Player.local;
+let focused = false;
+let isInVehicle = false;
+let stationsQueue = [];
+let audio;
+let output;
+let playing = 0;
+
+alt.onServer('playerEnteredVehicle', (vehicle, seat) => {
+
+    alt.emitServer('radio:GetRadioStations');
+
+    browser = new alt.WebView('http://resource/ui/radio.html');
+
+    pedInSeat = seat;
+    isInVehicle = true;
+    browser.on('radio:isplaying', (radiostat) => {
+        output = new alt.AudioOutputAttached(player.vehicle);
+        audio = new alt.Audio(radiostat,1,true);
+        audio.on("inited", () => { alt.log(`inited`); });
+        console.log(output.filter);
+        audio.addOutput(output);
+        audio.play();
+        playing = 1;
+            alt.log("YOUPI" + radiostat)
+        });
+        browser.on('radio:ismoving',() => {
+            if(playing == 1 && audio.playing == true){ alt.log("TYYE")
+            audio.pause()}
+            });
+    browser.on('radio:StationChanged', radioStation => {
+        
+        alt.emitServer('vehicle:RadioChanged', player.vehicle, radioStation);
+    });
+
+    browser.on('browser:mounted', () => {
+        mounted = true;
+
+        if (stationsQueue.length > 0) {
+            stationsQueue.forEach((station, index) => {
+                browser.emit('addRadioStation', station);
+                delete stationsQueue[index];
+            });
+        }
+
+        let currentVehicleRadio = player.vehicle.getSyncedMeta('radioStation')
+            ? player.vehicle.getSyncedMeta('radioStation')
+            : 0;
+        browser.emit('switchRadio', currentVehicleRadio);
+    });
+    
+    
+  
+});
+
+alt.onServer('playerLeftVehicle', (vehicle, seat) => {
+    // browser.unfocus();
+    // browser = undefined;
+    // pedInSeat = undefined;
+    // isInVehicle = false;
+    // mounted = false;
+});
+
+
+
+alt.onServer('radio:AddStation', station => {
+    if (mounted) {
+        browser.emit('addRadioStation', station);
+    } else {
+        stationsQueue.push(station);
+    }
+
+    alt.log(JSON.stringify(station));
+});
+
+alt.on('syncedMetaChange', (entity, key, value) => {
+    if (entity != player.vehicle || key != 'radioStation' || player.seat == 1) return;
+
+    if (browser && mounted)
+    
+        browser.emit('switchRadio', value);
+
+});
+
+alt.everyTick(() => {
+    if (isInVehicle) {
+        native.disableControlAction(0, 85, true);
+    } else {
+        native.enableControlAction(0, 85, false);
+    }
+
+    if (focused) {
+        native.disableControlAction(0, 99, true);
+        native.disableControlAction(0, 100, true);
+    } else {
+        native.enableControlAction(0, 99, true);
+        native.enableControlAction(0, 100, true);
+    }
+
+    if (native.isPedSittingInAnyVehicle(player.scriptID)) {
+        native.setRadioToStationName('OFF');
+    }
+});
+
+alt.on('keydown', key => {
+    if (native.isControlPressed(0, 152) == true && browser) {
+        
+        const pedInSeat = native.getPedInVehicleSeat(player.vehicle.scriptID, -1, false);
+        if (pedInSeat !== player.scriptID) return;
+
+        browser.focus();
+        focused = true;
+        browser.emit('focus');
+    }
+});
+
+alt.on('keyup', key => {
+    if (native.isControlPressed(0, 152) == true && browser) {
+        const pedInSeat = native.getPedInVehicleSeat(player.vehicle.scriptID, -1, false);
+        if (pedInSeat !== player.scriptID) return;
+
+        browser.unfocus();
+        focused = false;
+        setTimeout(() => {
+            if (browser)
+                browser.emit('unfocus');
+        }, 700);
+    }
+});
---
+++ server/config.mjs
@@ -0,0 +1,29 @@
+const radioList = [
+    //http://online-potok.ru/radio/ - Станции
+    {
+        name: 'NCS',
+        url: 'http://radio.gtrp.co:8030/radio.mp3',
+        image: 'http://resource/ui/img/011-fm.png',
+        volume: 100
+    },
+    {
+        name: 'Stayin Radio',
+        url: 'http://radio.gtrp.co:8020/radio.mp3',
+        image: 'http://resource/ui/img/rnr.png',
+        volume: 100
+    },
+    {
+        name: 'Rock',
+        url: 'http://radio.gtrp.co:8010/radio.mp3',
+        image: 'http://resource/ui/img/radiosa.png',
+        volume: 100
+    },
+    {
+        name: 'vicecity',
+        url: 'http://radio.gtrp.co:8060/radio.mp3',
+        image: 'http://resource/ui/img/msx.png',
+        volume: 100
+    }
+];
+
+export default radioList;
