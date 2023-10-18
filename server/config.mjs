e61b7e04ba20c6cdb6d52e06c749ab0abd6da22c
Jlawster <jordan.sellam.ort@gmail.com>
2023-10-18 02:29:18 +0200

    edit radio links

--- server/config.mjs
+++ server/config.mjs
@@ -2,7 +2,7 @@ const radioList = [
     //http://online-potok.ru/radio/ - Станции
     {
         name: 'TEST',
-        url: 'http://141.95.3.29:5478/listen/test/radio.mp3',
+        url: 'http://cdn.nrjaudio.fm/audio1/fr/40102/aac_576.mp3',
         image: 'http://resource/ui/img/011-fm.png',
         volume: 100
     },
